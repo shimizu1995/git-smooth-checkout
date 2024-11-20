713810d192a1f517ccb33cf79417a29e05abfa1b
Yuuki Shimizu <yu.shimizu@makip.co.jp>
2024-11-21 05:28:42 +0900

    Smoothly checkout in current git repository without repository selection, even in projects with submodules.

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { API } from "@/types/git";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-sample" is now active!');
+  const disposable = vscode.commands.registerCommand(
+    "git-smooth-checkout.checkout",
+    async () => {
+      const gitExtension = vscode.extensions.getExtension("vscode.git");
+      if (!gitExtension) {
+        vscode.window.showInformationMessage("vscode.git not found");
+        return;
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+      const api: API = gitExtension.exports.getAPI(1);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+      const selectedRepository = api.repositories.find(
+        (repo) => repo.ui.selected === true
+      );
+      vscode.commands.executeCommand("git.checkout", selectedRepository);
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
